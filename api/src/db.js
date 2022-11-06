10beefc78beaedcf20a458efe9fb9973b6667ea4
juan ricaud <ricaudjuan@hotmail.com>
2022-11-06 16:00:27 -0300

    env log

--- api/src/db.js
+++ api/src/db.js
@@ -6,9 +6,11 @@ const {
   DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_PORT
 } = process.env;
 
+console.log(DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_PORT, NODE_ENV) 
 let sequelize = 
   process.env.NODE_ENV === "production"
-    ? new Sequelize({
+    ?
+    new Sequelize({
       database: DB_NAME,
       dialect: "postgres",
       host: DB_HOST,
